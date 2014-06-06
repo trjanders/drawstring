1c27668faebe13e17f928708c08d32f4886bfb80
trjanders <trjanders@users.noreply.github.com>
2014-06-06 16:27:44 -0700

    updated filename to .java from .class

---
+++ CakeApplet.java
@@ -0,0 +1,11 @@
+//tja
+import java.applet.*;
+import java.awt.*;
+
+public class CakeApplet extends Applet {
+
+    public void paint(Graphics g) {
+            g.drawString("I am yummy a cake.",25, 25);
+    }
+		    
+}
